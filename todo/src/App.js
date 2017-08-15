b83f5c80ac546b9ed6ac7b381015f2814cd17ae7
Jim Unger <BigFunger@gmail.com>
2017-08-15 16:43:18 -0500

    Generating containers with connect() from react-redux - visible_todo_list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { VisibleTodoList } from './components/visible_todo_list';
+import { VisibleTodoList } from './containers/visible_todo_list';
 import { AddTodo } from './components/add_todo';
 import { Footer } from './components/footer';
 
